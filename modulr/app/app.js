ed62591b5f828898bd096b54477aee7d22ef4321
SELISE\shamim.ahmed <samimbit05@gmail.com>
2017-01-04 13:18:17 +0600

    no message

--- modulr/app/app.js
+++ modulr/app/app.js
@@ -1,15 +1,3 @@
 ﻿angular.module("sample", []);
 
-
-var app = angular.module("modulr", ['sample', 'ui.router']);
-
-app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
-    $locationProvider.html5Mode({
-        enabled: true,
-        requireBase: false
-    });
-    $stateProvider.state('sample', {
-        url: '/sample',
-        templateUrl: 'modules/sample/views/sample.view.html'
-    })
-})
+angular.module("modulr", ['sample', 'ui.router']);
---
+++ modulr/app/modules/main/main.config.js
@@ -0,0 +1,10 @@
+﻿angular.module('modulr').config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+    $stateProvider.state('sample', {
+        url: '/sample',
+        templateUrl: 'modules/sample/views/sample.view.html'
+    })
+})
